1dc31d642f4e55a96fc4708321fd98766e869441
ethanz2003 <ethanz2003@gmail.com>
2024-04-21 21:39:19 -0400

    Turned testing data into prompts for model

--- script/DJIA_testing_data.py
+++ script/DJIA_testing_data.py
@@ -31,16 +31,21 @@ print(f"Length of data: {len(djia_records)}")
 with open(output_file, 'w') as f:
     pass
 
+prompt_list = []
+
 for i in range(7, len(djia_records)):
     input_days = djia_records[i-7:i]
     output_day = djia_records[i]
     
+    
     # make each input a string and each output a string with the following format:
-    # input: 
-    # "Market Values for previous 6 days: 
-    # Day 1: Open:{open} High:{high} Low:{low} Close:{close} Adj Close:{adj close} Volume:{volume}, ... 
-    # Day 6: Open:{open} High:{high} Low:{low} Close:{close} Adj Close:{adj close} Volume:{volume}."
-    # output: "Today's Market Values: Open:{open} High:{high} Low:{low} Close:{close} Adj Close:{adj close} Volume:{volume}"
+    """Given the following financial input data for the past 6 days, only Output the Close value for day 7:
+
+    ### Financial Input Data:
+    Day 1: Open:{open} High:{high} Low:{low} Close:{close} Adj Close:{adj close} Volume:{volume}, ... 
+    # Day 6: Open:{open} High:{high} Low:{low} Close:{close} Adj Close:{adj close} Volume:{volume}.
+    ### Output Data:
+    """
     input_str = "Market Values for previous 6 days:"
     for j in range(6):
         input_str += (
@@ -53,20 +58,32 @@ for i in range(7, len(djia_records)):
             f"Close:{input_days[j]['Adj Close']} Volume:{input_days[j]['Volume']}."
         )
 
-    output_str = (
-        f"Today's Market Values: Open:{output_day['Open']} High:{output_day['High']} "
-        f"Low:{output_day['Low']} Close:{output_day['Close']} Adj "
-        f"Close:{output_day['Adj Close']} Volume:{output_day['Volume']}"
-    )
+    # output_str = (
+    #     f"Today's Market Values: Open:{output_day['Open']} High:{output_day['High']} "
+    #     f"Low:{output_day['Low']} Close:{output_day['Close']} Adj "
+    #     f"Close:{output_day['Adj Close']} Volume:{output_day['Volume']}"
+    # )
     
     # create a new record with the input and output strings
-    record = {"input": input_str, "output": output_str}
+    record = f"""Given the following financial input data for the past 6 days, only Output the Close value for day 7:
+    ### Financial Input Data:{input_str}
+    ### Output Data:"""
+    
+    # Append the record to the list
+    prompt_list.append(record)
     
     # print(str(record) + "\n")
 
-    # write the record to the JSON Lines file
-    with jsonlines.open(output_file, mode='a') as writer:
-        writer.write(record)
+# store the list of prompts into a json file
+with open("djia_stock_test_data_prompts.json", "w") as f:
+    json.dump(prompt_list, f)
+
+
+# check if the data is correct
+with open("djia_stock_test_data_prompts.json", 'r') as f:
+    prompt_list = json.load(f)
+    for prompt in prompt_list:
+        print(prompt)
 
 
 print(f"Data written to {output_file}")
