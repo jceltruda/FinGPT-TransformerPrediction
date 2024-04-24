5c67eb1b805397a23168f1ce6e77f9baf7b703c4
jceltruda <126628676+jceltruda@users.noreply.github.com>
2024-04-24 19:29:36 -0400

    Update TradingBot.py
    Implemented getData() and analyze()

--- TradingBot.py
+++ TradingBot.py
@@ -6,12 +6,44 @@ from datetime import datetime
 
 # Returns data needed by analyze function
 def getData(symbol):
-    return "Unimplemented"
+
+    currentDate = datetime.datetime.now()
+    year = currentDate.year
+    month = currentDate.month
+    day = currentDate.day
+    hour = currentDate.hour
+    second = currentDate.minute
+
+    request_params = StockTradesRequest(
+    symbol_or_symbols=symbol,
+    # Gets most recent trades
+        start=datetime(year, month, day, hour, second),
+    end=datetime(year, month, day, hour, second - datetime.timedelta(seconds=5))
+    )
+    
+    data_client = StockHistoricalDataClient("PK0X5HPYGPNMYOA5YL0O", "CBZbVoArVU7qrF2LLwHIeuNqnpLBL96Kj3S5Teig")
+    trades = data_client.get_stock_trades(request_params)
+    
+    currentPrice = trades[0]
+    predictedClose = getPrediction(symbol, currentPrice)
+    return currentPrice, predictedClose
+
+
+def getPrediction(symbol, actualPrice):
+     # Stub Coded
+     # Will integrate with google drive to use Fine-tuned model for stock prediction
+     return actualPrice
 
 # Function to determine if we should buy, sell, or hold
 def analyze(symbol):
-    data = getData(symbol)
-    qty = "Unimplemented"
+    currentPrice, predictedClose = getData(symbol)
+    delta = predictedClose - currentPrice
+    
+    # Change alpha to desired value
+    # Bot will sell/buy .02 shares for every dollar the price
+    # is expected to increase/decrease by
+    alpha = .02
+    qty = alpha * delta
     return qty
 
 if __name__ == '__main__':
